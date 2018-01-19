cb0a75fba8e0db0c9266f10d355a1439e49c468b
wintermao <winter_mao@163.com>
2018-01-19 14:07:46 +0800

    first version

---
+++ ddr_layout_V5/nios2/synthesis/submodules/nios2_addr_router_002.sv
@@ -0,0 +1,266 @@
+// (C) 2001-2013 Altera Corporation. All rights reserved.
+// Your use of Altera Corporation's design tools, logic functions and other 
+// software and tools, and its AMPP partner logic functions, and any output 
+// files any of the foregoing (including device programming or simulation 
+// files), and any associated documentation or information are expressly subject 
+// to the terms and conditions of the Altera Program License Subscription 
+// Agreement, Altera MegaCore Function License Agreement, or other applicable 
+// license agreement, including, without limitation, that your use is for the 
+// sole purpose of programming logic devices manufactured by Altera and sold by 
+// Altera or its authorized distributors.  Please refer to the applicable 
+// agreement for further details.
+
+
+// (C) 2001-2013 Altera Corporation. All rights reserved.
+// Your use of Altera Corporation's design tools, logic functions and other 
+// software and tools, and its AMPP partner logic functions, and any output 
+// files any of the foregoing (including device programming or simulation 
+// files), and any associated documentation or information are expressly subject 
+// to the terms and conditions of the Altera Program License Subscription 
+// Agreement, Altera MegaCore Function License Agreement, or other applicable 
+// license agreement, including, without limitation, that your use is for the 
+// sole purpose of programming logic devices manufactured by Altera and sold by 
+// Altera or its authorized distributors.  Please refer to the applicable 
+// agreement for further details.
+
+
+// $Id: //acds/rel/13.0/ip/merlin/altera_merlin_router/altera_merlin_router.sv.terp#1 $
+// $Revision: #1 $
+// $Date: 2013/02/11 $
+// $Author: swbranch $
+
+// -------------------------------------------------------
+// Merlin Router
+//
+// Asserts the appropriate one-hot encoded channel based on 
+// either (a) the address or (b) the dest id. The DECODER_TYPE
+// parameter controls this behaviour. 0 means address decoder,
+// 1 means dest id decoder.
+//
+// In the case of (a), it also sets the destination id.
+// -------------------------------------------------------
+
+`timescale 1 ns / 1 ns
+
+module nios2_addr_router_002_default_decode
+  #(
+     parameter DEFAULT_CHANNEL = 7,
+               DEFAULT_WR_CHANNEL = -1,
+               DEFAULT_RD_CHANNEL = -1,
+               DEFAULT_DESTID = 7 
+   )
+  (output [70 - 68 : 0] default_destination_id,
+   output [8-1 : 0] default_wr_channel,
+   output [8-1 : 0] default_rd_channel,
+   output [8-1 : 0] default_src_channel
+  );
+
+  assign default_destination_id = 
+    DEFAULT_DESTID[70 - 68 : 0];
+
+  generate begin : default_decode
+    if (DEFAULT_CHANNEL == -1) begin
+      assign default_src_channel = '0;
+    end
+    else begin
+      assign default_src_channel = 8'b1 << DEFAULT_CHANNEL;
+    end
+  end
+  endgenerate
+
+  generate begin : default_decode_rw
+    if (DEFAULT_RD_CHANNEL == -1) begin
+      assign default_wr_channel = '0;
+      assign default_rd_channel = '0;
+    end
+    else begin
+      assign default_wr_channel = 8'b1 << DEFAULT_WR_CHANNEL;
+      assign default_rd_channel = 8'b1 << DEFAULT_RD_CHANNEL;
+    end
+  end
+  endgenerate
+
+endmodule
+
+
+module nios2_addr_router_002
+(
+    // -------------------
+    // Clock & Reset
+    // -------------------
+    input clk,
+    input reset,
+
+    // -------------------
+    // Command Sink (Input)
+    // -------------------
+    input                       sink_valid,
+    input  [81-1 : 0]    sink_data,
+    input                       sink_startofpacket,
+    input                       sink_endofpacket,
+    output                      sink_ready,
+
+    // -------------------
+    // Command Source (Output)
+    // -------------------
+    output                          src_valid,
+    output reg [81-1    : 0] src_data,
+    output reg [8-1 : 0] src_channel,
+    output                          src_startofpacket,
+    output                          src_endofpacket,
+    input                           src_ready
+);
+
+    // -------------------------------------------------------
+    // Local parameters and variables
+    // -------------------------------------------------------
+    localparam PKT_ADDR_H = 45;
+    localparam PKT_ADDR_L = 36;
+    localparam PKT_DEST_ID_H = 70;
+    localparam PKT_DEST_ID_L = 68;
+    localparam PKT_PROTECTION_H = 74;
+    localparam PKT_PROTECTION_L = 72;
+    localparam ST_DATA_W = 81;
+    localparam ST_CHANNEL_W = 8;
+    localparam DECODER_TYPE = 0;
+
+    localparam PKT_TRANS_WRITE = 48;
+    localparam PKT_TRANS_READ  = 49;
+
+    localparam PKT_ADDR_W = PKT_ADDR_H-PKT_ADDR_L + 1;
+    localparam PKT_DEST_ID_W = PKT_DEST_ID_H-PKT_DEST_ID_L + 1;
+
+
+
+    // -------------------------------------------------------
+    // Figure out the number of bits to mask off for each slave span
+    // during address decoding
+    // -------------------------------------------------------
+    localparam PAD0 = log2ceil(64'h20 - 64'h0); 
+    localparam PAD1 = log2ceil(64'h40 - 64'h20); 
+    localparam PAD2 = log2ceil(64'h60 - 64'h40); 
+    localparam PAD3 = log2ceil(64'h70 - 64'h60); 
+    localparam PAD4 = log2ceil(64'h80 - 64'h70); 
+    localparam PAD5 = log2ceil(64'h90 - 64'h80); 
+    localparam PAD6 = log2ceil(64'h98 - 64'h90); 
+    localparam PAD7 = log2ceil(64'ha0 - 64'h98); 
+    // -------------------------------------------------------
+    // Work out which address bits are significant based on the
+    // address range of the slaves. If the required width is too
+    // large or too small, we use the address field width instead.
+    // -------------------------------------------------------
+    localparam ADDR_RANGE = 64'ha0;
+    localparam RANGE_ADDR_WIDTH = log2ceil(ADDR_RANGE);
+    localparam OPTIMIZED_ADDR_H = (RANGE_ADDR_WIDTH > PKT_ADDR_W) ||
+                                  (RANGE_ADDR_WIDTH == 0) ?
+                                        PKT_ADDR_H :
+                                        PKT_ADDR_L + RANGE_ADDR_WIDTH - 1;
+
+    localparam RG = RANGE_ADDR_WIDTH-1;
+
+      wire [PKT_ADDR_W-1 : 0] address = sink_data[OPTIMIZED_ADDR_H : PKT_ADDR_L];
+
+    // -------------------------------------------------------
+    // Pass almost everything through, untouched
+    // -------------------------------------------------------
+    assign sink_ready        = src_ready;
+    assign src_valid         = sink_valid;
+    assign src_startofpacket = sink_startofpacket;
+    assign src_endofpacket   = sink_endofpacket;
+
+    wire [PKT_DEST_ID_W-1:0] default_destid;
+    wire [8-1 : 0] default_src_channel;
+
+
+
+
+
+    nios2_addr_router_002_default_decode the_default_decode(
+      .default_destination_id (default_destid),
+      .default_wr_channel   (),
+      .default_rd_channel   (),
+      .default_src_channel  (default_src_channel)
+    );
+
+    always @* begin
+        src_data    = sink_data;
+        src_channel = default_src_channel;
+        src_data[PKT_DEST_ID_H:PKT_DEST_ID_L] = default_destid;
+
+        // --------------------------------------------------
+        // Address Decoder
+        // Sets the channel and destination ID based on the address
+        // --------------------------------------------------
+
+    // ( 0x0 .. 0x20 )
+    if ( {address[RG:PAD0],{PAD0{1'b0}}} == 8'h0   ) begin
+            src_channel = 8'b10000000;
+            src_data[PKT_DEST_ID_H:PKT_DEST_ID_L] = 7;
+    end
+
+    // ( 0x20 .. 0x40 )
+    if ( {address[RG:PAD1],{PAD1{1'b0}}} == 8'h20   ) begin
+            src_channel = 8'b01000000;
+            src_data[PKT_DEST_ID_H:PKT_DEST_ID_L] = 4;
+    end
+
+    // ( 0x40 .. 0x60 )
+    if ( {address[RG:PAD2],{PAD2{1'b0}}} == 8'h40   ) begin
+            src_channel = 8'b00001000;
+            src_data[PKT_DEST_ID_H:PKT_DEST_ID_L] = 6;
+    end
+
+    // ( 0x60 .. 0x70 )
+    if ( {address[RG:PAD3],{PAD3{1'b0}}} == 8'h60   ) begin
+            src_channel = 8'b00100000;
+            src_data[PKT_DEST_ID_H:PKT_DEST_ID_L] = 2;
+    end
+
+    // ( 0x70 .. 0x80 )
+    if ( {address[RG:PAD4],{PAD4{1'b0}}} == 8'h70   ) begin
+            src_channel = 8'b00010000;
+            src_data[PKT_DEST_ID_H:PKT_DEST_ID_L] = 3;
+    end
+
+    // ( 0x80 .. 0x90 )
+    if ( {address[RG:PAD5],{PAD5{1'b0}}} == 8'h80   ) begin
+            src_channel = 8'b00000001;
+            src_data[PKT_DEST_ID_H:PKT_DEST_ID_L] = 0;
+    end
+
+    // ( 0x90 .. 0x98 )
+    if ( {address[RG:PAD6],{PAD6{1'b0}}} == 8'h90   ) begin
+            src_channel = 8'b00000100;
+            src_data[PKT_DEST_ID_H:PKT_DEST_ID_L] = 1;
+    end
+
+    // ( 0x98 .. 0xa0 )
+    if ( {address[RG:PAD7],{PAD7{1'b0}}} == 8'h98   ) begin
+            src_channel = 8'b00000010;
+            src_data[PKT_DEST_ID_H:PKT_DEST_ID_L] = 5;
+    end
+
+end
+
+
+    // --------------------------------------------------
+    // Ceil(log2()) function
+    // --------------------------------------------------
+    function integer log2ceil;
+        input reg[65:0] val;
+        reg [65:0] i;
+
+        begin
+            i = 1;
+            log2ceil = 0;
+
+            while (i < val) begin
+                log2ceil = log2ceil + 1;
+                i = i << 1;
+            end
+        end
+    endfunction
+
+endmodule
+
+
---
+++ ddr_layout_V5/software/ddr_layout_V5_bsp/system.h
@@ -0,0 +1,426 @@
+/*
+ * system.h - SOPC Builder system and BSP software package information
+ *
+ * Machine generated for CPU 'nios2' in SOPC Builder design 'nios2'
+ * SOPC Builder design path: D:/altera/project/ddr_layout_V5/nios2.sopcinfo
+ *
+ * Generated: Fri Jan 19 11:02:12 CST 2018
+ */
+
+/*
+ * DO NOT MODIFY THIS FILE
+ *
+ * Changing this file will have subtle consequences
+ * which will almost certainly lead to a nonfunctioning
+ * system. If you do modify this file, be aware that your
+ * changes will be overwritten and lost when this file
+ * is generated again.
+ *
+ * DO NOT MODIFY THIS FILE
+ */
+
+/*
+ * License Agreement
+ *
+ * Copyright (c) 2008
+ * Altera Corporation, San Jose, California, USA.
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * This agreement shall be governed in all respects by the laws of the State
+ * of California and by the laws of the United States of America.
+ */
+
+#ifndef __SYSTEM_H_
+#define __SYSTEM_H_
+
+/* Include definitions from linker script generator */
+#include "linker.h"
+
+
+/*
+ * CPU configuration
+ *
+ */
+
+#define ALT_CPU_ARCHITECTURE "altera_nios2_qsys"
+#define ALT_CPU_BIG_ENDIAN 0
+#define ALT_CPU_BREAK_ADDR 0x11003820
+#define ALT_CPU_CPU_FREQ 108000000u
+#define ALT_CPU_CPU_ID_SIZE 1
+#define ALT_CPU_CPU_ID_VALUE 0x00000000
+#define ALT_CPU_CPU_IMPLEMENTATION "tiny"
+#define ALT_CPU_DATA_ADDR_WIDTH 0x1d
+#define ALT_CPU_DCACHE_LINE_SIZE 0
+#define ALT_CPU_DCACHE_LINE_SIZE_LOG2 0
+#define ALT_CPU_DCACHE_SIZE 0
+#define ALT_CPU_EXCEPTION_ADDR 0xc000020
+#define ALT_CPU_FLUSHDA_SUPPORTED
+#define ALT_CPU_FREQ 108000000
+#define ALT_CPU_HARDWARE_DIVIDE_PRESENT 0
+#define ALT_CPU_HARDWARE_MULTIPLY_PRESENT 0
+#define ALT_CPU_HARDWARE_MULX_PRESENT 0
+#define ALT_CPU_HAS_DEBUG_CORE 1
+#define ALT_CPU_HAS_DEBUG_STUB
+#define ALT_CPU_HAS_JMPI_INSTRUCTION
+#define ALT_CPU_ICACHE_LINE_SIZE 0
+#define ALT_CPU_ICACHE_LINE_SIZE_LOG2 0
+#define ALT_CPU_ICACHE_SIZE 0
+#define ALT_CPU_INST_ADDR_WIDTH 0x1d
+#define ALT_CPU_NAME "nios2"
+#define ALT_CPU_RESET_ADDR 0xc000000
+
+
+/*
+ * CPU configuration (with legacy prefix - don't use these anymore)
+ *
+ */
+
+#define NIOS2_BIG_ENDIAN 0
+#define NIOS2_BREAK_ADDR 0x11003820
+#define NIOS2_CPU_FREQ 108000000u
+#define NIOS2_CPU_ID_SIZE 1
+#define NIOS2_CPU_ID_VALUE 0x00000000
+#define NIOS2_CPU_IMPLEMENTATION "tiny"
+#define NIOS2_DATA_ADDR_WIDTH 0x1d
+#define NIOS2_DCACHE_LINE_SIZE 0
+#define NIOS2_DCACHE_LINE_SIZE_LOG2 0
+#define NIOS2_DCACHE_SIZE 0
+#define NIOS2_EXCEPTION_ADDR 0xc000020
+#define NIOS2_FLUSHDA_SUPPORTED
+#define NIOS2_HARDWARE_DIVIDE_PRESENT 0
+#define NIOS2_HARDWARE_MULTIPLY_PRESENT 0
+#define NIOS2_HARDWARE_MULX_PRESENT 0
+#define NIOS2_HAS_DEBUG_CORE 1
+#define NIOS2_HAS_DEBUG_STUB
+#define NIOS2_HAS_JMPI_INSTRUCTION
+#define NIOS2_ICACHE_LINE_SIZE 0
+#define NIOS2_ICACHE_LINE_SIZE_LOG2 0
+#define NIOS2_ICACHE_SIZE 0
+#define NIOS2_INST_ADDR_WIDTH 0x1d
+#define NIOS2_RESET_ADDR 0xc000000
+
+
+/*
+ * Define for each module class mastered by the CPU
+ *
+ */
+
+#define __ALTERA_AVALON_EPCS_FLASH_CONTROLLER
+#define __ALTERA_AVALON_JTAG_UART
+#define __ALTERA_AVALON_ONCHIP_MEMORY2
+#define __ALTERA_AVALON_PIO
+#define __ALTERA_AVALON_SPI
+#define __ALTERA_AVALON_SYSID_QSYS
+#define __ALTERA_AVALON_TIMER
+#define __ALTERA_AVALON_UART
+#define __ALTERA_NIOS2_QSYS
+#define __ALTMEMDDR2
+#define __ALTPLL
+
+
+/*
+ * System configuration
+ *
+ */
+
+#define ALT_DEVICE_FAMILY "Cyclone IV E"
+#define ALT_ENHANCED_INTERRUPT_API_PRESENT
+#define ALT_IRQ_BASE NULL
+#define ALT_LOG_PORT "/dev/null"
+#define ALT_LOG_PORT_BASE 0x0
+#define ALT_LOG_PORT_DEV null
+#define ALT_LOG_PORT_TYPE ""
+#define ALT_NUM_EXTERNAL_INTERRUPT_CONTROLLERS 0
+#define ALT_NUM_INTERNAL_INTERRUPT_CONTROLLERS 1
+#define ALT_NUM_INTERRUPT_CONTROLLERS 1
+#define ALT_STDERR "/dev/jtag_uart_0"
+#define ALT_STDERR_BASE 0x10000090
+#define ALT_STDERR_DEV jtag_uart_0
+#define ALT_STDERR_IS_JTAG_UART
+#define ALT_STDERR_PRESENT
+#define ALT_STDERR_TYPE "altera_avalon_jtag_uart"
+#define ALT_STDIN "/dev/jtag_uart_0"
+#define ALT_STDIN_BASE 0x10000090
+#define ALT_STDIN_DEV jtag_uart_0
+#define ALT_STDIN_IS_JTAG_UART
+#define ALT_STDIN_PRESENT
+#define ALT_STDIN_TYPE "altera_avalon_jtag_uart"
+#define ALT_STDOUT "/dev/jtag_uart_0"
+#define ALT_STDOUT_BASE 0x10000090
+#define ALT_STDOUT_DEV jtag_uart_0
+#define ALT_STDOUT_IS_JTAG_UART
+#define ALT_STDOUT_PRESENT
+#define ALT_STDOUT_TYPE "altera_avalon_jtag_uart"
+#define ALT_SYSTEM_NAME "nios2"
+
+
+/*
+ * altpll_0 configuration
+ *
+ */
+
+#define ALTPLL_0_BASE 0x10000080
+#define ALTPLL_0_IRQ -1
+#define ALTPLL_0_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define ALTPLL_0_NAME "/dev/altpll_0"
+#define ALTPLL_0_SPAN 16
+#define ALTPLL_0_TYPE "altpll"
+#define ALT_MODULE_CLASS_altpll_0 altpll
+
+
+/*
+ * ddr2 configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_ddr2 altmemddr2
+#define DDR2_BASE 0xc000000
+#define DDR2_IRQ -1
+#define DDR2_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define DDR2_NAME "/dev/ddr2"
+#define DDR2_SPAN 67108864
+#define DDR2_TYPE "altmemddr2"
+
+
+/*
+ * epcs_flash_controller_0 configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_epcs_flash_controller_0 altera_avalon_epcs_flash_controller
+#define EPCS_FLASH_CONTROLLER_0_BASE 0x11003000
+#define EPCS_FLASH_CONTROLLER_0_IRQ 5
+#define EPCS_FLASH_CONTROLLER_0_IRQ_INTERRUPT_CONTROLLER_ID 0
+#define EPCS_FLASH_CONTROLLER_0_NAME "/dev/epcs_flash_controller_0"
+#define EPCS_FLASH_CONTROLLER_0_REGISTER_OFFSET 1024
+#define EPCS_FLASH_CONTROLLER_0_SPAN 2048
+#define EPCS_FLASH_CONTROLLER_0_TYPE "altera_avalon_epcs_flash_controller"
+
+
+/*
+ * hal configuration
+ *
+ */
+
+#define ALT_MAX_FD 32
+#define ALT_SYS_CLK SYSTIMER
+#define ALT_TIMESTAMP_CLK none
+
+
+/*
+ * jtag_uart_0 configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_jtag_uart_0 altera_avalon_jtag_uart
+#define JTAG_UART_0_BASE 0x10000090
+#define JTAG_UART_0_IRQ 0
+#define JTAG_UART_0_IRQ_INTERRUPT_CONTROLLER_ID 0
+#define JTAG_UART_0_NAME "/dev/jtag_uart_0"
+#define JTAG_UART_0_READ_DEPTH 64
+#define JTAG_UART_0_READ_THRESHOLD 8
+#define JTAG_UART_0_SPAN 8
+#define JTAG_UART_0_TYPE "altera_avalon_jtag_uart"
+#define JTAG_UART_0_WRITE_DEPTH 64
+#define JTAG_UART_0_WRITE_THRESHOLD 8
+
+
+/*
+ * onchip_ram configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_onchip_ram altera_avalon_onchip_memory2
+#define ONCHIP_RAM_ALLOW_IN_SYSTEM_MEMORY_CONTENT_EDITOR 0
+#define ONCHIP_RAM_ALLOW_MRAM_SIM_CONTENTS_ONLY_FILE 0
+#define ONCHIP_RAM_BASE 0x11001000
+#define ONCHIP_RAM_CONTENTS_INFO ""
+#define ONCHIP_RAM_DUAL_PORT 0
+#define ONCHIP_RAM_GUI_RAM_BLOCK_TYPE "AUTO"
+#define ONCHIP_RAM_INIT_CONTENTS_FILE "nios2_onchip_ram"
+#define ONCHIP_RAM_INIT_MEM_CONTENT 1
+#define ONCHIP_RAM_INSTANCE_ID "NONE"
+#define ONCHIP_RAM_IRQ -1
+#define ONCHIP_RAM_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define ONCHIP_RAM_NAME "/dev/onchip_ram"
+#define ONCHIP_RAM_NON_DEFAULT_INIT_FILE_ENABLED 0
+#define ONCHIP_RAM_RAM_BLOCK_TYPE "AUTO"
+#define ONCHIP_RAM_READ_DURING_WRITE_MODE "DONT_CARE"
+#define ONCHIP_RAM_SINGLE_CLOCK_OP 0
+#define ONCHIP_RAM_SIZE_MULTIPLE 1
+#define ONCHIP_RAM_SIZE_VALUE 4096
+#define ONCHIP_RAM_SPAN 4096
+#define ONCHIP_RAM_TYPE "altera_avalon_onchip_memory2"
+#define ONCHIP_RAM_WRITABLE 1
+
+
+/*
+ * pio_key configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_pio_key altera_avalon_pio
+#define PIO_KEY_BASE 0x10000060
+#define PIO_KEY_BIT_CLEARING_EDGE_REGISTER 0
+#define PIO_KEY_BIT_MODIFYING_OUTPUT_REGISTER 0
+#define PIO_KEY_CAPTURE 1
+#define PIO_KEY_DATA_WIDTH 4
+#define PIO_KEY_DO_TEST_BENCH_WIRING 0
+#define PIO_KEY_DRIVEN_SIM_VALUE 0
+#define PIO_KEY_EDGE_TYPE "FALLING"
+#define PIO_KEY_FREQ 54000000
+#define PIO_KEY_HAS_IN 1
+#define PIO_KEY_HAS_OUT 0
+#define PIO_KEY_HAS_TRI 0
+#define PIO_KEY_IRQ 2
+#define PIO_KEY_IRQ_INTERRUPT_CONTROLLER_ID 0
+#define PIO_KEY_IRQ_TYPE "EDGE"
+#define PIO_KEY_NAME "/dev/pio_key"
+#define PIO_KEY_RESET_VALUE 0
+#define PIO_KEY_SPAN 16
+#define PIO_KEY_TYPE "altera_avalon_pio"
+
+
+/*
+ * pio_led configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_pio_led altera_avalon_pio
+#define PIO_LED_BASE 0x10000070
+#define PIO_LED_BIT_CLEARING_EDGE_REGISTER 0
+#define PIO_LED_BIT_MODIFYING_OUTPUT_REGISTER 0
+#define PIO_LED_CAPTURE 0
+#define PIO_LED_DATA_WIDTH 6
+#define PIO_LED_DO_TEST_BENCH_WIRING 0
+#define PIO_LED_DRIVEN_SIM_VALUE 0
+#define PIO_LED_EDGE_TYPE "NONE"
+#define PIO_LED_FREQ 54000000
+#define PIO_LED_HAS_IN 0
+#define PIO_LED_HAS_OUT 1
+#define PIO_LED_HAS_TRI 0
+#define PIO_LED_IRQ -1
+#define PIO_LED_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define PIO_LED_IRQ_TYPE "NONE"
+#define PIO_LED_NAME "/dev/pio_led"
+#define PIO_LED_RESET_VALUE 0
+#define PIO_LED_SPAN 16
+#define PIO_LED_TYPE "altera_avalon_pio"
+
+
+/*
+ * spi_0 configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_spi_0 altera_avalon_spi
+#define SPI_0_BASE 0x10000020
+#define SPI_0_CLOCKMULT 1
+#define SPI_0_CLOCKPHASE 0
+#define SPI_0_CLOCKPOLARITY 0
+#define SPI_0_CLOCKUNITS "Hz"
+#define SPI_0_DATABITS 16
+#define SPI_0_DATAWIDTH 16
+#define SPI_0_DELAYMULT "1.0E-9"
+#define SPI_0_DELAYUNITS "ns"
+#define SPI_0_EXTRADELAY 0
+#define SPI_0_INSERT_SYNC 0
+#define SPI_0_IRQ 4
+#define SPI_0_IRQ_INTERRUPT_CONTROLLER_ID 0
+#define SPI_0_ISMASTER 1
+#define SPI_0_LSBFIRST 0
+#define SPI_0_NAME "/dev/spi_0"
+#define SPI_0_NUMSLAVES 1
+#define SPI_0_PREFIX "spi_"
+#define SPI_0_SPAN 32
+#define SPI_0_SYNC_REG_DEPTH 2
+#define SPI_0_TARGETCLOCK 1000000u
+#define SPI_0_TARGETSSDELAY "0.0"
+#define SPI_0_TYPE "altera_avalon_spi"
+
+
+/*
+ * sysid configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_sysid altera_avalon_sysid_qsys
+#define SYSID_BASE 0x10000098
+#define SYSID_ID 0
+#define SYSID_IRQ -1
+#define SYSID_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define SYSID_NAME "/dev/sysid"
+#define SYSID_SPAN 8
+#define SYSID_TIMESTAMP 1515746610
+#define SYSID_TYPE "altera_avalon_sysid_qsys"
+
+
+/*
+ * systimer configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_systimer altera_avalon_timer
+#define SYSTIMER_ALWAYS_RUN 0
+#define SYSTIMER_BASE 0x10000040
+#define SYSTIMER_COUNTER_SIZE 32
+#define SYSTIMER_FIXED_PERIOD 0
+#define SYSTIMER_FREQ 54000000
+#define SYSTIMER_IRQ 1
+#define SYSTIMER_IRQ_INTERRUPT_CONTROLLER_ID 0
+#define SYSTIMER_LOAD_VALUE 539999
+#define SYSTIMER_MULT 0.0010
+#define SYSTIMER_NAME "/dev/systimer"
+#define SYSTIMER_PERIOD 10
+#define SYSTIMER_PERIOD_UNITS "ms"
+#define SYSTIMER_RESET_OUTPUT 0
+#define SYSTIMER_SNAPSHOT 1
+#define SYSTIMER_SPAN 32
+#define SYSTIMER_TICKS_PER_SEC 100.0
+#define SYSTIMER_TIMEOUT_PULSE_OUTPUT 0
+#define SYSTIMER_TYPE "altera_avalon_timer"
+
+
+/*
+ * uart_0 configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_uart_0 altera_avalon_uart
+#define UART_0_BASE 0x10000000
+#define UART_0_BAUD 115200
+#define UART_0_DATA_BITS 8
+#define UART_0_FIXED_BAUD 1
+#define UART_0_FREQ 54000000
+#define UART_0_IRQ 3
+#define UART_0_IRQ_INTERRUPT_CONTROLLER_ID 0
+#define UART_0_NAME "/dev/uart_0"
+#define UART_0_PARITY 'N'
+#define UART_0_SIM_CHAR_STREAM ""
+#define UART_0_SIM_TRUE_BAUD 0
+#define UART_0_SPAN 32
+#define UART_0_STOP_BITS 1
+#define UART_0_SYNC_REG_DEPTH 2
+#define UART_0_TYPE "altera_avalon_uart"
+#define UART_0_USE_CTS_RTS 0
+#define UART_0_USE_EOP_REGISTER 0
+
+#endif /* __SYSTEM_H_ */
