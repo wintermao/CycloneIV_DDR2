1c60b4171c509e12e0524b8d7594924c6b1f1853
wintermao <winter_mao@163.com>
2018-04-14 14:47:46 +0800

    修改clk_gen模块为支持16路时钟输出，最小2分频的自然数分频，同步更新cl_gen_tb，用于仿真

--- ddr_layout_V5/ip/clk_gen/hdl/clk_gen.v
+++ ddr_layout_V5/ip/clk_gen/hdl/clk_gen.v
@@ -1,82 +1,43 @@
-//create by mmh 2018.3.4
+//create by mmh 2018.3.22
 
-`define	AddrAD1			3'h00
-`define	AddrAD2			3'h01
-`define AddrDAC1		2'h02
-`define AddrDAC2		3'h03
-`define AddrDAC3		3'h04
-`define	AddrDAC4		3'h05
-`define	AddrLight		3'h06
 
 module clk_gen					
-(
+(clk_source,clk_out,clk,reset_n,chipselect,address,read,write,readdata,writedata);
+	parameter clk_num=4;
 	//clock output interface
-	input  wire clk_source,				//clock sourece
-	output reg ad1_clk,						//ad1 clock
-	output reg ad2_clk,						//ad2 clock
-	output reg dac1_clk,					//dac1 clock
-	output reg dac2_clk,					//dac2 clock
-	output reg dac3_clk,					//dac3 clock
-	output reg dac4_clk,					//dac4 clock
-	output reg light_clk,					//light clock
+	input  wire clk_source;						//clock sourece
+	output reg [clk_num-1:0] clk_out;	//clock output
 	
 	//avalon interface
-	input  wire clk,							//avalon clock
-	input  wire reset_n,					//avalon reset_n
-	input  wire chipselect,				//avalon chipselect
-	input  wire [2:0] address,		//avalon address
-	input  wire read,							//avalon read
-	input  wire write,						//avalon write
-	output reg  [31:0] readdata,	//avalon read data
-	input  wire [31:0] writedata	//avalon write data
-);
-	reg	[31:0] ad1_pre,ad1_num;
-	reg [31:0] ad2_pre,ad2_num;
-	reg [31:0] dac1_pre,dac1_num;
-	reg [31:0] dac2_pre,dac2_num;
-	reg [31:0] dac3_pre,dac3_num;
-	reg [31:0] dac4_pre,dac4_num;
-	reg [31:0] light_pre,light_num;
+	input  wire clk;							//avalon clock
+	input  wire reset_n;					//avalon reset_n
+	input  wire chipselect;				//avalon chipselect
+	input  wire [4:0] address;		//avalon address
+	input  wire read;							//avalon read
+	input  wire write;						//avalon write
+	output reg  [31:0] readdata;	//avalon read data
+	input  wire [31:0] writedata;	//avalon write data
 
-	parameter OSC_CLOCK=108000000;
+	
+	reg	[31:0] clk_pre[0:clk_num-1];	//predivide
+	reg [31:0] clk_count[0:clk_num-1];	//counter
+	integer i;
 
 	always @(posedge clk or negedge reset_n)
 	begin
 		if (!reset_n)
 		begin
-			ad1_pre<=1;
-			ad2_pre<=1;
-			dac1_pre<=1;
-			dac2_pre<=1;
-			dac3_pre<=1;
-			dac4_pre<=1;
-			light_pre<=OSC_CLOCK/2;
+			for(i=0;i<clk_num;i=i+1) clk_pre[i]<=2+i;
 		end
 		else
 		begin
 			if(chipselect && write)		//avalon write 
 			begin
-				case(address)
-					`AddrAD1:		ad1_pre <= writedata;
-					`AddrAD2:		ad2_pre <= writedata;
-					`AddrDAC1:	dac1_pre <= writedata;
-					`AddrDAC2:	dac2_pre <= writedata;
-					`AddrDAC3:	dac3_pre <= writedata;
-					`AddrDAC4:	dac4_pre <= writedata;
-					`AddrLight:	light_pre <= writedata;
-				endcase
+				if(address<clk_num) clk_pre[address]<=writedata;
 			end
 			else if(chipselect && read)
 			begin
-				case(address)
-					`AddrAD1:		readdata <= ad1_pre;
-					`AddrAD2:		readdata <= ad2_pre;
-					`AddrDAC1:	readdata <= dac1_pre;
-					`AddrDAC2:	readdata <= dac2_pre;
-					`AddrDAC3:	readdata <= dac3_pre;
-					`AddrDAC4:	readdata <= dac4_pre;
-					`AddrLight:	readdata <= light_pre;
-				endcase
+				if(address<clk_num) readdata<=clk_pre[address];
 			end
 		end
 	end
@@ -85,99 +46,18 @@ module clk_gen
 	begin
 		if (!reset_n)
 		begin
-			ad1_num<=0;
-			ad2_num<=0;
-			dac1_num<=0;
-			dac2_num<=0;
-			dac3_num<=0;
-			dac4_num<=0;
-			light_num<=0;
-			ad1_clk<=0;
-			ad2_clk<=0;
-			dac1_clk<=0;
-			dac2_clk<=0;
-			dac3_clk<=0;
-			dac4_clk<=0;
-			light_clk<=0;
+			for(i=0;i<clk_num;i=i+1) clk_count[i]<=0;
 		end
 		else
 		begin
-			ad1_num<=ad1_num+1;
-			ad2_num<=ad2_num+1;
-			dac1_num<=dac1_num+1;
-			dac2_num<=dac2_num+1;
-			dac3_num<=dac3_num+1;
-			dac4_num<=dac4_num+1;
-			light_num<=light_num+1;
-			
-			if(ad1_num>=ad1_pre) 
-			begin
-				ad1_num<=0;
-				ad1_clk <= ~ad1_clk;
-			end 
-			else 
-			begin
-				ad1_clk <= ad1_clk;
-			end
-			
-			if(ad2_num>=ad2_pre) 
-			begin
-				ad2_num<=0;
-				ad2_clk <= ~ad2_clk;
-			end 
-			else 
-			begin
-				ad2_clk <= ad2_clk;
-			end
-			
-			if(dac1_num>=dac1_pre) 
-			begin
-				dac1_num<=0;
-				dac1_clk <= ~dac1_clk;
-			end 
-			else 
-			begin
-				dac1_clk <= dac1_clk;
-			end
-			
-			if(dac2_num>=dac2_pre) 
-			begin
-				dac2_num<=0;
-				dac2_clk <= ~dac2_clk;
-			end 
-			else 
-			begin
-				dac2_clk <= dac2_clk;
-			end
-			
-			if(dac3_num>=dac3_pre) 
-			begin
-				dac3_num<=0;
-				dac3_clk <= ~dac3_clk;
-			end 
-			else 
-			begin
-				dac3_clk <= dac3_clk;
-			end
-			
-			if(dac4_num>=dac4_pre) 
-			begin
-				dac4_num<=0;
-				dac4_clk <= ~dac4_clk;
-			end 
-			else 
-			begin
-				dac4_clk <= dac4_clk;
-			end
-			
-			if(light_num>=light_pre) 
-			begin
-				light_num<=0;
-				light_clk <= ~light_clk;
-			end 
-			else 
-			begin
-				light_clk <= light_clk;
+			for(i=0;i<clk_num;i=i+1) begin
+				if(clk_count[i]==clk_pre[i]-1) clk_count[i]<=0;
+				else clk_count[i]<=clk_count[i]+1;
+				//if(clk_count[i]==clk_pre[i]-1) clk_out[i] <= ~clk_out[i];
+				//else if(clk_count[i]==clk_pre[i]>>1) clk_out[i] <= ~clk_out[i];
+				if(clk_count[i]==0) clk_out[i] <= 0;
+				else if(clk_count[i]==clk_pre[i]>>1) clk_out[i] <= 1;
+				else clk_out[i] <= clk_out[i];
 			end
 		end
 	end
--- ddr_layout_V5/ip/clk_gen/hdl/clk_gen_tb.v
+++ ddr_layout_V5/ip/clk_gen/hdl/clk_gen_tb.v
@@ -8,20 +8,39 @@
  `define delay 5
  
  module clk_gen_tb;
- localparam clk_num=4;
- 	reg clk_source,clk,reset_n,chipselectread,write;
+ parameter clk_num=4;
+ 	reg clk_source,clk,reset_n,chipselect,read,write;
  	reg [31:0] writedata;
- 	reg [3:0] address;
+ 	reg [4:0] address;
  	wire	[clk_num-1:0] clk_out;
  	wire [31:0] readdata;					
  clk_gen tb(clk_source,clk_out,clk,reset_n,chipselect,address,read,write,readdata,writedata);
-
+  defparam tb.clk_num=clk_num;
 	// Generates serial clock of time period 10
  initial                         
    begin
      clk_source = 0;
      forever #`delay clk_source = !clk_source;
    end
+ initial                         
+   begin
+     clk=0;
+     forever #`delay clk = !clk;
+   end
+task  avalon_write;
+  input [4:0] addr;
+  input [31:0] data;
+  begin
+  #`delay;
+	address=addr;
+	writedata=data;
+	chipselect=1;
+	write=1;
+	#`delay;
+	chipselect=0;
+	write=0;
+	end
+endtask
    
  initial                         
    begin
@@ -30,6 +49,11 @@
      reset_n = 0;
      #`delay
      reset_n = 1;
+     
+     avalon_write(5'h0,32'h2);
+     avalon_write(5'h1,32'h3); 
+     avalon_write(5'h2,32'h4); 
+     avalon_write(5'h3,32'h5); 
      
    end
 
