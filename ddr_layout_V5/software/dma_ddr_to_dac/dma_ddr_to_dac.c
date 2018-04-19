b6fdd57dbcd15ed06d10c27f8b186ad7378722b1
wintermao <winter_mao@163.com>
2018-04-19 13:52:05 +0800

    1 更新dac2904 ip核，增加waitrequest信号，用于解决慢速设备的速率匹配问题 2 更新系统工程，由于dac2094新模块增加了采样时钟输入引脚 3 更新dma_ddr_to_dac工程，用于测试传输速率 4 更新dma测试结果

--- ddr_layout_V5/software/dma_ddr_to_dac/dma_ddr_to_dac.c
+++ ddr_layout_V5/software/dma_ddr_to_dac/dma_ddr_to_dac.c
@@ -15,41 +15,54 @@
 #include "sys/alt_dma.h"
 #include "clk_gen.h"
 #include "clk_device.h"
+#include "math.h"
+#define PI 3.14159265359
 
 static volatile int tx_done = 0;
-
+alt_u32 *ddr_dword1,*ddr_dword2;
+alt_u32 offset_source,offset_dest,base_source,base_dest,size_byte;
+alt_dma_txchan txchan;
 //callback funtion
 static void done (void* handle)
 {
     tx_done=1;
+    dma_init(txchan,ddr_dword1,ddr_dword2,size_byte);
 }
-
+void dma_init(alt_dma_txchan txchan,alt_u32 *s_addr,alt_u32 *d_addr,alt_u32 t_size)
+{
+	alt_dma_txchan_ioctl(txchan,  ALT_DMA_TX_ONLY_ON, d_addr);
+	alt_dma_txchan_send(txchan, s_addr,	 t_size, done, NULL);
+}
 int main()
 {
 	clk_gen_dev dev_clk;
 	alt_u32 t0,t1,dt;
   alt_u32 i,timestamp_freq;
-  alt_u32 *ddr_dword1,*ddr_dword2;
-  alt_u32 offset_source,offset_dest,base_source,base_dest,size_byte;
+
   //创建DMA信道
-  alt_dma_txchan txchan;
+
   //blink led
+  alt_u32 divide;
+  float a,b,step;
+
+  divide=8;
   clk_gen_init(&dev_clk,CLK_GEN_BASE);
   clk_gen_write(&dev_clk,LIGHT,ALT_CPU_CPU_FREQ);
-  clk_gen_write(&dev_clk,DAC1,54);	//set DA freq to 1M
+  clk_gen_write(&dev_clk,DAC1,divide);	//set DA freq to 1M
 
   //DDR2_BASE
   base_source=DDR2_BASE;
   base_dest=DAC2904_1_BASE;
   offset_source=0x1000000;
   offset_dest=0x000000;
-  size_byte=0x1000;
+  size_byte=0x10000;
 
   ddr_dword1=base_source+offset_source;
   ddr_dword2=base_dest+offset_dest;
+  step=PI*2/size_byte*4*256;
 	for(i=0;i<size_byte/4;i++)		//generate data,size is 1M byte
 	{
-	  *ddr_dword1=i*16;
+	  *ddr_dword1=(int)((sin(step*i)+1)*0x2000);
 	  ddr_dword1++;
 	}
   timestamp_freq=alt_timestamp_freq();
@@ -70,6 +83,8 @@ int main()
 	ddr_dword1=base_source+offset_source;
 	alt_dma_txchan_ioctl(txchan,  ALT_DMA_SET_MODE_32, NULL);
 	alt_dma_txchan_ioctl(txchan,  ALT_DMA_TX_ONLY_ON, ddr_dword2);
+	dma_init(txchan,ddr_dword1,ddr_dword2,size_byte);
+	/*
 	if (alt_dma_txchan_send(    txchan,
 							 ddr_dword1,
 							 size_byte,
@@ -84,17 +99,20 @@ int main()
 	 t0 = alt_timestamp();
 	 printf("start send.\n");
 	}
+	*/
      //-----------------------------------------------------------
      /* 等待发送结束 */
 	/*
      while (!tx_done);
      t1 = alt_timestamp();
      printf ("Transfer successful!\n");
+     printf("divide freq=%d\n",divide);
      dt=t1-t0;
      printf("dma dword transmit: size=0x%x t0=%ld t1=%ld dt=%ld time=%fs\n", size_byte,t0,t1,dt,(double)dt/timestamp_freq);
-     */
+
      //-----------------------------------------------------------
      //关闭DMA接收信道
+
 	while(1)
 	{
 		int i;
@@ -110,6 +128,11 @@ int main()
 			i++;
 			printf("dma transmit successful ,count=%5d\n",i);
 		}
+	}
+	*/
+	while(1)
+	{
+
 	}
      alt_dma_txchan_close(txchan);
 
